f4cc2e3d6b4631bac22c321c8b6ea7d4ece87ec6
ntmanh90 <ntmanh90ksth@gmail.com>
2021-10-12 11:52:24 +0700

    c

--- index.js
+++ index.js
@@ -14,31 +14,30 @@ import logData from './app/utils/logData';
 import AppApi from './app/api/Client';
 import {openDatabase} from 'react-native-sqlite-storage';
 import moment from 'moment';
-import {isIOS} from 'react-native-elements/dist/helpers';
 
 var db = openDatabase({name: 'UserDatabase.db'});
 var conn = getHubAndReconnect();
 // BackgroundTimer.start();
 
-if (!isIOS) {
-  conn.off('IncomingCallAsterisk');
-  conn.on('IncomingCallAsterisk', (callid, number, displayname, data, id) => {
+conn.off('IncomingCallAsterisk');
+conn.on(
+  'IncomingCallAsterisk',
+  async (callid, number, displayname, data, id) => {
     conn
       .invoke('ConfirmEvent', 'IncomingCallAsterisk', callid)
       .catch(error => console.log(error));
+    RNCallKeep.backToForeground();
     logData.writeLogData('[[on] | IncomingCallAsterisk], index: ' + number);
     let sdt_incoming = number;
-    storeData.getStoreDataValue(keyStoreData.Prefix).then(prefix => {
-      sdt_incoming = number.replace(prefix, '');
-      storeData.setStoreDataValue(keyStoreData.soDienThoaiDen, sdt_incoming);
-    });
-
+    let prefix = await storeData.getStoreDataValue(keyStoreData.Prefix);
+    sdt_incoming = number.replace(prefix, '');
+    storeData.setStoreDataValue(keyStoreData.soDienThoaiDen, sdt_incoming);
     var signal = JSON.parse(data);
     storeData.setStoreDataObject(keyStoreData.signalWebRTC, signal);
     storeData.setStoreDataValue(keyStoreData.callid, callid);
     DeviceEventEmitter.emit('displayIncomingCallEvent');
-  });
-}
+  },
+);
 
 const sendLog = async () => {
   let somayle = await storeData.getStoreDataValue(keyStoreData.somayle);
@@ -105,64 +104,55 @@ const sendLog = async () => {
 // Register background handler
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('[remoteMessage index]', remoteMessage);
-  conn = getHubAndReconnect();
-  if (!isIOS) {
-    if (remoteMessage.data.type == 'wakeup') {
-      if (!isIOS) {
-        logData.writeLogData('[Wakeup]');
-        let paramNotiData = {
-          uniqueid: remoteMessage.data.uniqueid,
-          channel: remoteMessage.data.channel,
-          thoiGianCuocGoiDen: moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
-        };
-        storeData.setStoreDataObject(keyStoreData.paramNoti, paramNotiData);
+  if (remoteMessage.data.type == 'wakeup') {
+    conn = getHubAndReconnect();
+    logData.writeLogData('[Wakeup]');
+    let paramNotiData = {
+      uniqueid: remoteMessage.data.uniqueid,
+      channel: remoteMessage.data.channel,
+      thoiGianCuocGoiDen: moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+    };
+    storeData.setStoreDataObject(keyStoreData.paramNoti, paramNotiData);
 
-        let http = await storeData.getStoreDataValue(keyStoreData.urlApi);
-        var url = http + 'redirect';
-        console.log(url);
+    let http = await storeData.getStoreDataValue(keyStoreData.urlApi);
+    var url = http + 'redirect';
+    console.log(url);
 
-        NetInfo.fetch().then(async state => {
-          let idct = await storeData.getStoreDataValue(keyStoreData.idct);
-          let mact = await storeData.getStoreDataValue(keyStoreData.tenct);
-          let idnhanvien = await storeData.getStoreDataValue(
-            keyStoreData.idnhanvien,
-          );
-          let ext = await storeData.getStoreDataValue(keyStoreData.somayle);
-          let deviceName = await DeviceInfo.getDeviceName();
-          var params = {
-            mact: mact,
-            ext: ext,
-            channel: remoteMessage.data.channel,
-            uniqueid: remoteMessage.data.uniqueid,
-            internet: state.type,
-            idct: idct,
-            idnhanvien: idnhanvien,
-            ver: BaseURL.VERSION,
-            os: Platform.OS == 'ios' ? 1 : 2,
-            dongmay: DeviceInfo.getBrand(),
-            imei: DeviceInfo.getUniqueId(),
-            devicename: deviceName,
-            osversion: DeviceInfo.getSystemVersion(),
-          };
+    NetInfo.fetch().then(async state => {
+      let idct = await storeData.getStoreDataValue(keyStoreData.idct);
+      let mact = await storeData.getStoreDataValue(keyStoreData.tenct);
+      let idnhanvien = await storeData.getStoreDataValue(
+        keyStoreData.idnhanvien,
+      );
+      let ext = await storeData.getStoreDataValue(keyStoreData.somayle);
+      let deviceName = await DeviceInfo.getDeviceName();
+      var params = {
+        mact: mact,
+        ext: ext,
+        channel: remoteMessage.data.channel,
+        uniqueid: remoteMessage.data.uniqueid,
+        internet: state.type,
+        idct: idct,
+        idnhanvien: idnhanvien,
+        ver: BaseURL.VERSION,
+        os: Platform.OS == 'ios' ? 1 : 2,
+        dongmay: DeviceInfo.getBrand(),
+        imei: DeviceInfo.getUniqueId(),
+        devicename: deviceName,
+        osversion: DeviceInfo.getSystemVersion(),
+      };
 
-          AppApi.RequestPOST(url, params, (err, json) => {
-            if (!err) {
-              if (json.data.status) {
-                logData.writeLogData(
-                  '[CallAPI: redirect]: Result' +
-                    JSON.stringify(json.data.status),
-                );
-              } else {
-              }
-            }
-          });
-        });
-      }
-    }
+      AppApi.RequestPOST(url, params, (err, json) => {
+        if (!err) {
+          if (json.data.status)
+            logData.writeLogData(
+              '[CallAPI: redirect]: Result' + JSON.stringify(json.data.status),
+            );
+        }
+      });
+    });
   }
-
   if (remoteMessage.data.type == 'DangXuat') {
-    logData.writeLogData('Firebase Dang Xuat');
     storeData.setStoreDataValue(keyStoreData.isLogin, false);
     storeData.setStoreDataObject('sip_user', {});
     storeData.setStoreDataValue('tennhanvien', '');
@@ -172,11 +162,5 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
     sendLog();
   }
 });
-
-if (isIOS) {
-  messaging()
-    .getIsHeadless()
-    .then(isHeadless => {});
-}
 
 AppRegistry.registerComponent(appName, () => App);
